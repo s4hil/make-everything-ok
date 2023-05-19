7d1da32c65df86b0b2d06721693278ec2e7de1e6
s4hil <s4hilp4rr4y@gmail.com>
2023-05-19 18:48:07 +0530

    music

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $("#btn").click(()=>{
+
+	let music = document.getElementById('song');
+
+	music.play();
+
+
 	setInterval(function () {
 		$("#progress-modal").show();
 		let width = $(".progress").width();
 		if(width < 540){
-			let newWidth = width + 30 ;
+			let newWidth = width + 27;
 			$(".progress").css('width', newWidth);
 		}
 		else {
